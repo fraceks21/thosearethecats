f3f6a6cc4c350614cd55037a84a7f8c4cd8669c9
fraceks21 <31926604+fraceks21@users.noreply.github.com>
2025-05-14 08:54:53 -0400

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://cekky-eightcatsrollingdownthestairs.hf.space"
+PASSWORD = "frc"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
